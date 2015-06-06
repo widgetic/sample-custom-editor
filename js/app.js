881de495efe208a09e6493438b6a45fb736c796b
Cristian Necula <cristian.necula@vuzum.com>
2015-06-06 14:54:19 +0300

    Added comments

--- js/app.js
+++ js/app.js
@@ -2,16 +2,13 @@
   var CustomEditor = function(el) {
     this.el = el;
 
+    // initialize the component
     this.widgetArea = this.el.find('.widget-area');
     this.urlInput   = this.el.find('[name=url]');
     this.content    = [];
     this.order      = 0;
 
-    this.widgetArea.css({
-      width: 500,
-      height: 400
-    })
-    
+    // bind the event handlers
     this.el.on('input', '.skin-control',   this._onSkinControlInput.bind(this))
     this.el.on('click', '[name=clear]',    this._onClearClicked.bind(this))
     this.el.on('click', '[name=add]',      this._onAddClicked.bind(this))
@@ -58,6 +55,7 @@
   };
 
   CustomEditor.prototype._onSkinControlInput = function(ev) {
+    // check if the preview has been created
     if(!this.preview) return;
 
     // mark that the skin has been changed
@@ -65,54 +63,75 @@
 
     var $input = $(ev.currentTarget);
 
+    // create the skinChanges object
+    // @example: { backgroundColor: '#ffaaff' }
     var skinChanges = {};
     skinChanges[$input.attr('name')] = $input.val();
 
+    // apply the changes
     this.preview.changeSkin(skinChanges);
   }
 
   CustomEditor.prototype._onClearClicked = function() {
+    // empty the preview widget
     this.preview.clearContent();
+
+    // update the custom editor's list of content items
     this.content = [];
     this._renderContentList();
   }
 
   CustomEditor.prototype._onAddClicked = function() {
+    // validate the input
     var imageUrl = this.urlInput.val();
     if(!/^https?:\/\/(?:[a-z\-]+\.)+[a-z]{2,6}(?:\/[^\/#?]+)+/.test(imageUrl)) {
       alert("Does not look like a valid url");
       return;
     }
-    var newContent = {
-      id: +new Date(),
-      order: this.order++,
-      image: imageUrl
-    };
-    this.content.push(newContent);
-    this.preview.addContent(newContent);
-    this._renderContentList();
-  }
 
-  CustomEditor.prototype._renderContentList = function() {
-    var $list = this.el.find('.content-list')
-    var html = this.content.reduce(function(acc, cur) {
-      console.log(cur, acc);
-      return acc + 
-        '<li data-id="'+ cur.id +'">'+ cur.image.match(/(\/.+?)+$/)[1] +' <a href="#">Remove</a></li>';
-    }, '')
-    $list.html(html);
+    // create a content object according to the content schema
+    // @see http://docs.widgetic.com/#the-content-meta
+    var newContent = {
+      id: +new Date(),     // set a unique id to be able to reference it later
+      order: this.order++, // specify the order in wich the content items are displayed
+      image: imageUrl      // set the mainAttribute
+      // add the rest of the content properties
+    };
+    
+    // add the content to the widget
+    this.preview.addContent(newContent);
+
+    // update the custom editors' list of content items
+    this.content.push(newContent);
+    this._renderContentList();
   }
 
   CustomEditor.prototype._onRemoveClicked = function(ev) {
     ev.preventDefault();
+
+    // get the id of the content
     var id = $(ev.currentTarget).parents('li').data('id')
 
+    // remove the content from the preview referencing it by id
     this.preview.removeContent({id: id});
+
+    // update the custom editors' list of content items    
     this.content = this.content.filter(function(item) {
       return item.id !== id;
     })
     this._renderContentList();
   };
+
+  CustomEditor.prototype._renderContentList = function() {
+    var $list = this.el.find('.content-list')
+    var html = this.content.reduce(function(acc, cur) {
+      return acc + 
+        '<li data-id="'+ cur.id +'">'+ 
+        cur.image.match(/(\/.+?)+$/)[1] +
+        ' <a href="#">Remove</a></li>';
+    }, '')
+    $list.html(html);
+  }
 
   var App = function(el) {
     this.el = el;
